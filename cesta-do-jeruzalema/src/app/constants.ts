43a0e72183ffe6ffdcf1b86ae1de5b56bd24975a
9motom6 <9motom6@gmail.com>
2021-03-10 22:41:14 +0100

    Fix time again

--- cesta-do-jeruzalema/src/app/constants.ts
+++ cesta-do-jeruzalema/src/app/constants.ts
@@ -1,4 +1,4 @@
 export const START_DATE: Date = new Date(2021, 2, 6, 0, 0, 0);
-export const END_DATE: Date = new Date(2021, 3, 2, 23, 59, 59);
+export const END_DATE: Date = new Date(2021, 3, 2, 24, 60, 0);
 export const TOTAL_DISTANCE = 3143;
 export const GOOGLE_MAP_LINK = "https://goo.gl/maps/Bod72y9vn2ev6bKk9";
